99e2275796e8cbffbf82318eebc12eced81dbf63
Salih <salih.hajlakovic@gmail.com>
2016-02-22 22:02:34 +0100

    Simple app Multiplie - initial commit

--- Multiplies/ViewController.swift
+++ Multiplies/ViewController.swift
@@ -10,16 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Outlets
+    @IBOutlet weak var imgViewMultiples: UIImageView!
+    @IBOutlet weak var lblAdd: UILabel!
+    @IBOutlet weak var txtAdd: UITextField!
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var btnAdd: UIButton!
+    
+    //Variables
+    var tempRes = 0
+    var add = 0
+    var maxResult = 0
+    var isFirstIteration = true
+    
+    let lblText = "Press Add to add!"
+    let txtPlaceholderText = "What multiple to add by?"
+    
+    //Actions
+    
+    @IBAction func Play(sender: UIButton) {
+        setVisibility(true)
+        setEquation()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func Add(sender: UIButton)
+    {
+        setEquation()
     }
-
+    
+    func setVisibility(isVisible: Bool)
+    {
+        imgViewMultiples.hidden = isVisible
+        txtAdd.hidden = isVisible
+        btnPlay.hidden = isVisible
+    
+        lblAdd.hidden = !isVisible
+        btnAdd.hidden = !isVisible
+    }
+    
+    func setEquation()
+    {
+        if(tempRes < maxResult || isFirstIteration)
+        {
+            if (isFirstIteration){
+                add = Int(txtAdd.text!)!
+                maxResult = add * add
+                isFirstIteration = false
+            }
+        
+            lblAdd.text = "\(tempRes) + \(add) = \(tempRes + add)"
+            tempRes += add
+        }
+        else
+        {
+            ResetGame()
+        }
+    }
+    
+    func ResetGame()
+    {
+        resetScoresAndText()
+        setVisibility(false)
+    }
+    
+    func resetScoresAndText()
+    {
+        txtAdd.placeholder = txtPlaceholderText
+        txtAdd.text = ""
+        lblAdd.text = lblText
+        
+        tempRes = 0
+        add = 0
+        maxResult = 0
+        isFirstIteration = true
+    }
 
 }
 
